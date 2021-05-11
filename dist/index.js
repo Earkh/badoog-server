55c00a94e55fb9cfb2629c45a7e9249bba0fba0b
Airam <airam.dev@gmail.com>
2021-05-11 22:08:09 +0100

    Modified CORS

--- dist/index.js
+++ dist/index.js
@@ -11,13 +11,6 @@ const cors_1 = __importDefault(require("cors"));
 const user_1 = __importDefault(require("./routes/user"));
 const swipe_1 = __importDefault(require("./routes/swipe"));
 const server = new server_1.default();
-server.app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 // BODY PARSER
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
--- index.ts
+++ index.ts
@@ -10,13 +10,7 @@ import swipeRoutes from "./routes/swipe";
 
 
 const server = new Server();
-server.app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+
 // BODY PARSER
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
